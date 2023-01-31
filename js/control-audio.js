a8e7a807719eb0686d38714ecc7a4f22e6d45ea1
AdrielStuart23 <adrielstuart9@gmail.com>
2023-01-31 12:14:58 -0400

    Aprende con stu, representacion de la web, sin funcionalidad completa

---
+++ js/control-audio.js
@@ -0,0 +1,22 @@
+
+function botonPlaya() {
+
+   let video = document.getElementById("videA")
+   video.play()
+}
+function botonPlaye() {
+    let video = document.getElementById("videE")
+    video.play()
+}
+function botonPlayi() {
+    let video = document.getElementById("videI")
+    video.play()
+}
+function botonPlayo() {
+    let video = document.getElementById("videO")
+    video.play()
+}
+function botonPlayu() {
+    let video = document.getElementById("videU")
+    video.play()
+}
